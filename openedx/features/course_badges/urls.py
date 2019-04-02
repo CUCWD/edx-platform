950509f90ddafe0f2b8404e9e5f44f0d247f5fb5
Zachary Trabookis <ztraboo@clemson.edu>
2019-04-02 11:46:17 -0400

    Added plugin for `course_badges` to openedx features. EW-405
    This provides a plugin extension for badges where a learner can view their badge progress for within a course.

--- openedx/features/course_badges/urls.py
+++ openedx/features/course_badges/urls.py
@@ -3,18 +3,36 @@ Defines URLs for course badges.
 """
 
 from django.conf.urls import url
+from django.views.generic.base import RedirectView
 
-from views.course_badges import CourseBadgesFragmentView, CourseBadgesView
+from views.course_badges_home import CourseBadgesHomeFragmentView, CourseBadgesHomeView
+from views.course_badges_progress import CourseBadgesProgressFragmentView, CourseBadgesProgressView
 
 urlpatterns = [
     url(
-        r'^$',
-        CourseBadgesView.as_view(),
-        name='openedx.course_badges.home',
+        r'^progress$',
+        CourseBadgesProgressView.as_view(),
+        name='openedx.course_badges.progress',
     ),
     url(
-        r'^badges_fragment$',
-        CourseBadgesFragmentView.as_view(),
-        name='openedx.course_badges.course_badges_fragment_view',
+        r'^badges_progress_fragment$',
+        CourseBadgesProgressFragmentView.as_view(),
+        name='openedx.course_badges.course_badges_progress_fragment_view',
+    ),
+    # Todo: Should we want to actually have a home page for the Badges MFE we'll need to enable these urls.
+    # url(
+    #     r'^$',
+    #     CourseBadgesHomeView.as_view(),
+    #     name='openedx.course_badges.home',
+    # ),
+    # url(
+    #     r'^badges_home_fragment$',
+    #     CourseBadgesHomeFragmentView.as_view(),
+    #     name='openedx.course_badges.course_badges_home_fragment_view',
+    # ),
+    url(
+        r'^$',
+        RedirectView.as_view(pattern_name='openedx.course_badges.progress', permanent=False),
+        name='openedx.course_badges.home',
     ),
 ]
