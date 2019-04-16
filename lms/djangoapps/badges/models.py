d2c52a3a74e9563e39618bec7e6c5688b4c3fd44
Zachary Trabookis <ztraboo@clemson.edu>
2019-04-16 16:45:06 -0400

    Resolve merge conflict issues with `develop` branch.

--- lms/djangoapps/badges/models.py
+++ lms/djangoapps/badges/models.py
@@ -341,6 +341,10 @@ class BlockEventBadgesConfiguration(models.Model):
     )
 
     @classmethod
+    def config_for_block_event(cls, course_id, event_type):
+        pass
+    
+    @classmethod
     def get_badgeclass_for_chapter_complete(cls, course_id='', usage_key=''):
         """
         Return all records matching course identifier and event type.
