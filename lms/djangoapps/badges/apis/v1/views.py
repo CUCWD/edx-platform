49706c066c7e4298bb98b4e6f597fe2af013bf6d
Zachary Trabookis <ztraboo@clemson.edu>
2020-09-30 09:47:09 -0400

    Adding `authentication_classes` to `UserBadgeProgressListView`. EW-535

--- lms/djangoapps/badges/apis/v1/views.py
+++ lms/djangoapps/badges/apis/v1/views.py
@@ -513,6 +513,12 @@ class UserBadgeProgressListView(BadgeProgressViewMixin, APIView):
             ...
         ]
     """
+    authentication_classes = (
+        JwtAuthentication,
+        BearerAuthenticationAllowInactiveUser,
+        SessionAuthenticationAllowInactiveUser,
+    )
+    
     permission_classes = (permissions.JWT_RESTRICTED_APPLICATION_OR_USER_ACCESS,)
 
     def get(self, request, course_id=None, username=None, *args, **kwargs):  # pylint: disable=arguments-differ        
