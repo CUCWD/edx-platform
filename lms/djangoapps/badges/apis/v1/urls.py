cbf83108134e96aa6d074644c9f1f5801bc82a23
Zachary Trabookis <ztraboo@clemson.edu>
2021-03-24 15:04:35 -0400

    Updated the `Badges` API endpoints to accommodate the new MFE `frontend-app-learning`.
    Added in `course_home_api` endpoint for `course_metadata` for MFE course navigation elements even though this is not being used by the `frontend-app-learning` yet for this Juniper release.

--- lms/djangoapps/badges/apis/v1/urls.py
+++ lms/djangoapps/badges/apis/v1/urls.py
@@ -2,35 +2,42 @@
 URLs for badges API
 """
 
-
+from django.urls import re_path
 from django.conf import settings
 from django.conf.urls import include, url
 
 from badges.apis.v1 import views
 
-BADGES_URLS = ([
-    url(
-        r'^assertions/user/{username}/$'.format(
+app_name = 'v1'
+urlpatterns = []
+
+urlpatterns += [
+    re_path(
+        r'assertions/user/{username}$'.format(
             username=settings.USERNAME_PATTERN
         ),
-        views.UserBadgeAssertions.as_view(), name='user_assertions'
+        views.UserBadgeAssertions.as_view(),
+        name='badges-user-assertions'
     ),
-    url(
-        r'^progress/courses/{course_id}/$'.format(
+]
+
+urlpatterns += [
+    re_path(
+        r'progress/courses/{course_id}$'.format(
             course_id=settings.COURSE_ID_PATTERN
         ),
-        views.CourseBadgeProgressListView.as_view(), name='course_progress'
+        views.CourseBadgeProgressListView.as_view(),
+        name='badges-course-progress'
     ),
-    url(
-        r'^progress/courses/{course_id}/user/{username}/$'.format(
+]
+
+urlpatterns += [
+    re_path(
+        r'progress/courses/{course_id}/user/{username}$'.format(
             course_id=settings.COURSE_ID_PATTERN,
             username=settings.USERNAME_PATTERN
         ),
-        views.UserBadgeProgressListView.as_view(), name='course_progress_user'
+        views.UserBadgeProgressListView.as_view(),
+        name='badges-course-progress-user'
     ),
-], 'badges')
-
-app_name = 'v1'
-urlpatterns = [
-    url(r'^', include(BADGES_URLS)),
 ]
