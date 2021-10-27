affb9ad9b110cb684d6d7dc887942b5d587fb639
Zachary Trabookis <ztraboo@clemson.edu>
2021-10-27 18:53:44 -0400

    Fix issue with `third_party_auth` database migration not finding dependency.

--- common/djangoapps/third_party_auth/migrations/0003_auto_20211023_1607.py
+++ common/djangoapps/third_party_auth/migrations/0003_auto_20211023_1607.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('third_party_auth', '0002_emailproviderconfig'),
+        ('third_party_auth', '0022_emailproviderconfig'),
     ]
 
     operations = [
