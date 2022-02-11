6a5902e4affcda4d183345e83e25ca4d3f93d28d
Zachary Trabookis <ztraboo@clemson.edu>
2022-02-11 11:30:30 -0500

    feat: Setup REST endpoint to find user badge progress. EW-409

--- lms/djangoapps/badges/api/urls.py
+++ lms/djangoapps/badges/api/urls.py
@@ -6,9 +6,20 @@ URLs for badges API
 from django.conf import settings
 from django.urls import re_path
 
-from .views import UserBadgeAssertions
+from .views import UserBadgeAssertions, UserBadgeProgress
 
 urlpatterns = [
-    re_path('^assertions/user/' + settings.USERNAME_PATTERN + '/$',
-            UserBadgeAssertions.as_view(), name='user_assertions'),
+    re_path(fr'^assertions/user/{settings.USERNAME_PATTERN}/$',
+        UserBadgeAssertions.as_view(), name='user_assertions'
+        ),
+    re_path(fr'^assertions/user/{settings.USERNAME_PATTERN}/$',
+        UserBadgeAssertions.as_view(), name='user_assertions'
+        ),
+    # re_path(fr'^progress/user/{settings.USERNAME_PATTERN}/$',
+    #     UserBadgeProgress.as_view(), name='user_progress'
+    #     ),
+    re_path(
+        fr'^progress/user/{settings.USERNAME_PATTERN}/courses/{settings.COURSE_ID_PATTERN}/$',
+        UserBadgeProgress.as_view(), name='user_progress'
+    ),
 ]
