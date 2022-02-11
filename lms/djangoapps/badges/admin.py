c02b2bfcdca7925724cd5c00c182dc58ca4ab0bf
Zachary Trabookis <ztraboo@clemson.edu>
2022-02-11 11:16:55 -0500

    feat: Setup block id event configuration model for badging. EW-409
    This badging configuration model is to be used with assigning course events to BadgeClass models. When a chapter is complete we would award this configured BadgeClass model to the learner.

--- lms/djangoapps/badges/admin.py
+++ lms/djangoapps/badges/admin.py
@@ -6,11 +6,12 @@ Admin registration for Badge Models
 from config_models.admin import ConfigurationModelAdmin
 from django.contrib import admin
 
-from lms.djangoapps.badges.models import (
+from lms.djangoapps.badges.models import (  # pylint: disable=syntax-error
     BadgeAssertion,
     BadgeClass,
     CourseCompleteImageConfiguration,
-    CourseEventBadgesConfiguration
+    CourseEventBadgesConfiguration,
+    BlockEventBadgesConfiguration
 )
 
 admin.site.register(CourseCompleteImageConfiguration)
@@ -18,3 +19,14 @@ admin.site.register(BadgeClass)
 admin.site.register(BadgeAssertion)
 # Use the standard Configuration Model Admin handler for this model.
 admin.site.register(CourseEventBadgesConfiguration, ConfigurationModelAdmin)
+
+@admin.register(BlockEventBadgesConfiguration)
+class BlockEventBadgesConfigurationAdmin(admin.ModelAdmin):
+    """
+    Admin model for managing block event badge configurations.
+    Mapping course modules blocks to badges classes.
+    """
+    list_display = ('course_id', 'event_type', 'badge_class', 'usage_key')
+    list_filter = ('course_id', 'event_type')
+    ordering = ('course_id', 'event_type', 'badge_class')
+    search_fields = ('course_id', 'event_type')
