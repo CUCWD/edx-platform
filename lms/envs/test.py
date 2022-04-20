1e3f63737b1bd9ad6f7e8afd274fb6c95f13f3da
Zachary Trabookis <ztraboo@clemson.edu>
2022-04-20 09:57:47 -0400

    test(analytics): Updated LMS environment `test.py` for BigCommerce and Badges.

--- lms/envs/test.py
+++ lms/envs/test.py
@@ -603,21 +603,64 @@ CORS_ORIGIN_WHITELIST = ['https://sandbox.edx.org']
 
 #################### OpenBadges Settings #######################
 
+FEATURES['ENABLE_OPENBADGES'] = True
+
+# Be sure to set up images for course modes using the BadgeImageConfiguration model in
+# the certificates app.
+# BADGR_API_VERSION = "v2"
+# BADGR_API_TOKEN_EXPIRATION = 86400
+# BADGR_API_TOKEN = "FymTGk7wQ3hR268JwS00ngkefdA0GR" # "0WVtn1RWangTSBqf5DDNvEdMTCvSL0"
+# BADGR_API_REFRESH_TOKEN = "F0HDDPNHK1ZBnSVXpMYHslRiD6Qdir" # Ur7ZWamo8pwaPkK3dim124ak7c1YPG"
+
+BADGR_USERNAME = "cucwd.developer@gmail.com"
+BADGR_PASSWORD = "Ca2ves2018"
+BADGR_TOKENS_CACHE_KEY = "badgr_api_token_cache"
 BADGR_API_TOKEN_CACHE = 'default'
+
+# Do not add the trailing slash here.
+BADGR_BASE_URL = "https://api.badgr.io"
+BADGR_ISSUER_SLUG = "abcdefghijklmnopqrstuv"  # This override defined in Site Configuration
+
+# Number of seconds to wait on the badging server when contacting it before giving up.
+# Already set in edx-platform/lms/envs/common.py
+# BADGR_TIMEOUT = 10
+
+# Only for v1 Badgr API
+# BADGR_API_NOTIFICATIONS_ENABLED = True
+
+BADGR_PUBLIC_URL = "https://badgr.io"
+
+# if FEATURES.get('ENABLE_OPENBADGES'):
+#     course_badges_templates_dir = OPENEDX_ROOT / 'features' / 'course_badges' / 'templates'
+#     if course_badges_templates_dir not in MAKO_TEMPLATE_DIRS_BASE:
+#         MAKO_TEMPLATE_DIRS_BASE.insert(0, course_badges_templates_dir)
+
+BADGR_ENABLE_NOTIFICATIONS = True
 
 #################### BigCommerce Settings #######################
 
-FEATURES.update({
-
-    # Enable BigCommerce feature.
-    'ENABLE_BIGCOMMERCE': True,
-
-})
+FEATURES['ENABLE_BIGCOMMERCE'] = False
 
 if FEATURES.get('ENABLE_BIGCOMMERCE'):
-    BIGCOMMERCE_APP_CLIENT_ID = "6ms4rvrkhnv5m3h1o582mtqb7wzixyr"
-    BIGCOMMERCE_APP_CLIENT_SECRET = "385d434a82fe40cd838ad5891bdbc1548209547112f1e81c4b16bc2842d1a329"
-    BIGCOMMERCE_APP_STORE_HASH = "1nol3cto8"
-    BIGCOMMERCE_APP_STORE_URL = "https://educateworkforce-development.mybigcommerce.com"
+    BIGCOMMERCE_APP_ACCESS_TOKEN="rg2zptq3s9x1nzh8sttgy8zcjgd9gyu"
+    BIGCOMMERCE_APP_CLIENT_ID="6ms4rvrkhnv5m3h1o582mtqb7wzixyr"
+    BIGCOMMERCE_APP_CLIENT_SECRET="385d434a82fe40cd838ad5891bdbc1548209547112f1e81c4b16bc2842d1a329"
+    BIGCOMMERCE_APP_STORE_HASH="1nol3cto8"
+    BIGCOMMERCE_APP_STORE_URL="https://educateworkforce-development.mybigcommerce.com"
 
     INSTALLED_APPS.append('bigcommerce')
+    # INSTALLED_APPS.append('bigcommerce_app')
+
+    INSTALLED_APPS.append('social_auth_backend_bigcommerce')
+
+    ##### Third-party auth options
+    # Order of AUTHENTICATION_BACKENDS matters. See https://docs.djangoproject.com/en/2.2/topics/auth/customizing/#specifying-authentication-backends  # lint-amnesty, pylint: disable=line-too-long
+    ################################################
+    if FEATURES.get('ENABLE_THIRD_PARTY_AUTH'):
+        tmp_backends = [
+            'social_auth_backend_bigcommerce.backend.BigCommerceCustomerDefaultAuth',
+            'social_auth_backend_bigcommerce.backend.BigCommerceCustomerTrustworksAuth',
+        ]
+
+        AUTHENTICATION_BACKENDS = list(tmp_backends) + list(AUTHENTICATION_BACKENDS)
+        del tmp_backends
