c40727dd2da91824f13daf10daa569359724fada
Chris Gaber <cgaber0@gmail.com>
2022-05-03 14:45:38 -0400

    feat(estimated-time): Updated UI and youtube api pull

--- cms/djangoapps/contentstore/courseware_index.py
+++ cms/djangoapps/contentstore/courseware_index.py
@@ -78,21 +78,50 @@ def get_estimated_time(cls, modulestore, structure_key):
                     if xblock.category == 'video':
                         vid_time = 240
                         vid_url = xblock.edx_video_id
-                        if xblock.edx_video_id != '':
+
+                        if xblock.youtube_id_1_0 != '' and settings.YOUTUBE_API_KEY != "PUT_YOUR_API_KEY_HERE":
+                            try:
+                                import urllib.request
+                                import json
+                                import isodate
+
+                                vid_id = xblock.youtube_id_1_0
+                                api_url="https://www.googleapis.com/youtube/v3/videos?id="+vid_id+"&key="+settings.YOUTUBE_API_KEY+"&part=contentDetails"
+
+                                req = urllib.request.Request(api_url, headers={'User-Agent': 'Mozilla/5.0'})
+                                with urllib.request.urlopen(req) as url:
+                                    data = json.loads(url.read().decode())
+
+                                vid_time = data['items'][0]['contentDetails']['duration']
+                                vid_time = isodate.parse_duration(vid_time).total_seconds()
+                            except Exception as e:
+                                print(e)
+                            
+                        elif xblock.edx_video_id != '':
                             vid_info = edxval_api.get_video_info(vid_url)
                             vid_time = vid_info.get("duration", 240)
-                        
-                        xblock.estimated_time = datetime.timedelta(seconds=vid_time)
+
+                        xblock.estimated_time = datetime.timedelta(seconds=int(vid_time))
                     
                     # if the xblock is a drag and drop change the estimated time to 5 minutes
                     if xblock.category == 'drag-and-drop-v2' and xblock.override_estimated_time == False \
                     and xblock.estimated_time == datetime.timedelta(minutes=1):
                         xblock.estimated_time = datetime.timedelta(minutes=5)
 
-                        # if the block is open response change the estimated time to 10 minutes
+                    # if the block is open response change the estimated time to 10 minutes
                     if xblock.category == 'openassessment' and xblock.override_estimated_time == False \
                     and xblock.estimated_time == datetime.timedelta(minutes=1):
                         xblock.estimated_time = datetime.timedelta(minutes=10)
+
+                    # if the block is open response change the estimated time to 10 minutes
+                    if xblock.category == 'openassessment' and xblock.override_estimated_time == False \
+                    and xblock.estimated_time == datetime.timedelta(minutes=1):
+                        xblock.estimated_time = datetime.timedelta(minutes=10)
+
+                    # if the block is a multiple choice change the estimated time to the number of problems times 1 minute
+                    if xblock.category == 'problem' and xblock.override_estimated_time == False \
+                    and xblock.estimated_time == datetime.timedelta(minutes=1):
+                        xblock.estimated_time = datetime.timedelta(minutes=len(xblock.problem_types))
                     
                     unit_time += xblock.estimated_time
                     modulestore().update_item(xblock, None)
