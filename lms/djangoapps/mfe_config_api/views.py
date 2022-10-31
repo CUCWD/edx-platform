81a7801eea00f5bc857971fa6b14b5d4debe588a
Zachary Trabookis <ztraboo@clemson.edu>
2022-10-31 12:55:12 -0400

    feat(config): Update `mfe_runtime` api endpoint to use HTTP Referrer to locate correct Site Configuration.

--- lms/djangoapps/mfe_config_api/views.py
+++ lms/djangoapps/mfe_config_api/views.py
@@ -2,6 +2,10 @@
 MFE API Views for useful information related to mfes.
 """
 
+import json
+import logging
+import re
+
 from django.conf import settings
 from django.http import HttpResponseNotFound, JsonResponse
 from django.utils.decorators import method_decorator
@@ -11,13 +15,18 @@ from rest_framework.views import APIView
 
 from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
 
+from openedx.core.djangoapps.site_configuration.models import (
+    SiteConfiguration
+)
+
+log = logging.getLogger(__name__)
 
 class MFEConfigView(APIView):
     """
     Provides an API endpoint to get the MFE_CONFIG from site configuration.
     """
 
-    @method_decorator(cache_page(settings.MFE_CONFIG_API_CACHE_TIMEOUT))
+    # @method_decorator(cache_page(settings.MFE_CONFIG_API_CACHE_TIMEOUT))
     def get(self, request):
         """
         GET /api/v1/mfe_config
@@ -43,10 +52,52 @@ class MFEConfigView(APIView):
         if not settings.ENABLE_MFE_CONFIG_API:
             return HttpResponseNotFound()
 
-        mfe_config = configuration_helpers.get_value('MFE_CONFIG', getattr(settings, 'MFE_CONFIG', {}))
-        if request.query_params.get('mfe'):
-            mfe = str(request.query_params.get('mfe')).upper()
-            mfe_config.update(configuration_helpers.get_value(
-                f'MFE_CONFIG_{mfe}', getattr(settings, f'MFE_CONFIG_{mfe}', {})))
+        # Remove the protocol and split the url at the slashes. Return the first entry for the domain name.
+        mfe_config = {}
 
+        log.info(
+            "HTTP Referer %s", request.META.get('HTTP_REFERER')
+        )
+        log.info(
+            "request.META %s", request.META
+        )
+        if request.META.get('HTTP_REFERER'):
+            referer = re.sub('^https?:\/\/', '', request.META.get('HTTP_REFERER')).split('/')[0]
+
+            for site_config in SiteConfiguration.objects.all():            
+                mfe_config = site_config.site_values.get("MFE_CONFIG", {})
+                if mfe_config.get("BASE_URL"):
+                    mfe_config_base_url = re.sub('^https?:\/\/', '', mfe_config.get("BASE_URL")).split('/')[0]
+                
+                    if mfe_config_base_url == referer:
+                        log.info(
+                            "Found the site configuration that matches the MFE base domain."
+                        )
+
+                        configuration = getattr(site_config.site, "configuration", None)
+
+                        mfe_config = configuration.get_value(
+                            f'MFE_CONFIG', getattr(settings, f'MFE_CONFIG', {}))
+
+                        if request.query_params.get('mfe'):
+                            mfe = str(request.query_params.get('mfe')).upper()
+
+                            
+                            mfe_config.update(configuration.get_value(
+                                f'MFE_CONFIG_{mfe}', getattr(settings, f'MFE_CONFIG_{mfe}', {})))
+
+                        # Exit out of the loop once you find first correct
+                        # MFE_CONFIG in Site Configuration.
+                        break;
+        else:
+            mfe_config = configuration_helpers.get_value('MFE_CONFIG', getattr(settings, 'MFE_CONFIG', {}))
+
+            if request.query_params.get('mfe'):
+                mfe = str(request.query_params.get('mfe')).upper()
+                mfe_config.update(configuration_helpers.get_value(
+                    f'MFE_CONFIG_{mfe}', getattr(settings, f'MFE_CONFIG_{mfe}', {})))
+
+        log.info(
+            "mfe_config = %s", mfe_config
+        )
         return JsonResponse(mfe_config, status=status.HTTP_200_OK)
