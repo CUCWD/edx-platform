ee750edf135f2b375483c33a6496ab5410d8f465
Zachary Trabookis <ztraboo@clemson.edu>
2022-11-03 09:33:57 -0400

    feat(config): Updated MFE frontend paths to use SiteConfiguration values.

--- openedx/core/djangoapps/user_api/accounts/settings_views.py
+++ openedx/core/djangoapps/user_api/accounts/settings_views.py
@@ -58,12 +58,12 @@ def account_settings(request):
 
     """
     if should_redirect_to_account_microfrontend():
-        url = settings.ACCOUNT_MICROFRONTEND_URL
+        settings_account_microfrontend_url = configuration_helpers.get_value('ACCOUNT_MICROFRONTEND_URL', settings.ACCOUNT_MICROFRONTEND_URL)
 
         duplicate_provider = pipeline.get_duplicate_provider(messages.get_messages(request))
         if duplicate_provider:
             url = '{url}?{params}'.format(
-                url=url,
+                url=settings_account_microfrontend_url,
                 params=urllib.parse.urlencode({
                     'duplicate_provider': duplicate_provider,
                 }),
