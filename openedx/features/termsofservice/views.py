124f2cb53e775990a2167789e1f46bc4f2b1473f
Divyanshu <div@duck.com>
2023-02-16 11:37:53 -0500

    feat: Code Review changes for more error handling

--- openedx/features/termsofservice/views.py
+++ openedx/features/termsofservice/views.py
@@ -5,6 +5,9 @@ from django.contrib.sites.models import Site
 from django.template.loader import render_to_string
 from django.conf import settings
 from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def latest_terms_of_service():  # lint-amnesty, pylint: disable=missing-function-docstring
@@ -13,7 +16,11 @@ def latest_terms_of_service():  # lint-amnesty, pylint: disable=missing-function
         cur_site_id = Site.objects.get(domain=site_name)
         cur_site_curf_id = TermsOfServiceSites.objects.get(site_id=cur_site_id.id).curf_id
     else:
-        cur_site_curf_id = TermsOfServiceAllSites.objects.all().first().curf_id
+        try:
+            cur_site_curf_id = TermsOfServiceAllSites.objects.all().first().curf_id
+        except TermsOfServiceAllSites.DoesNotExist:
+            tos_html = "Need to setup a Terms of Service Acknowledgment for " + site_name
+            logger.info('Need to setup a Terms of Service Acknowledgment for {site_name}')
 
     tos_html = TermsOfService.objects.get(curf_id=cur_site_curf_id).terms_of_service_text
 
