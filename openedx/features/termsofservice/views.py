985ed19f6c535ed3c6ead956264ae76d7cec4a5a
Zachary Trabookis <ztraboo@clemson.edu>
2023-02-22 23:56:18 -0500

    feat: Add some error handling when `TermsOfService` models are empty.

--- openedx/features/termsofservice/views.py
+++ openedx/features/termsofservice/views.py
@@ -12,20 +12,26 @@ logger = logging.getLogger(__name__)
 
 def latest_terms_of_service():  # lint-amnesty, pylint: disable=missing-function-docstring
     site_name = configuration_helpers.get_value("SITE_NAME", settings.SITE_NAME)
+    msg = (
+        f"Need to setup a Terms of Service Acknowledgment for {site_name}"
+    )
+    tos_html = msg
+    cur_site_curf_id = None
+
     if settings.FEATURES.get('ENABLE_TERMSOFSERVICE_PER_SUBSITE'):
-        cur_site_id = Site.objects.get(domain=site_name)
-        cur_site_curf_id = TermsOfServiceSites.objects.get(site_id=cur_site_id.id).curf_id
+        try:
+            cur_site_id = Site.objects.get(domain=site_name)
+            cur_site_curf_id = TermsOfServiceSites.objects.get(site_id=cur_site_id.id).curf_id
+        except (TermsOfServiceSites.DoesNotExist, AttributeError) as error:
+            logger.info(f"{msg}\n{str(error)}")
     else:
         try:
             cur_site_curf_id = TermsOfServiceAllSites.objects.all().first().curf_id
-
-            tos_html = TermsOfService.objects.get(curf_id=cur_site_curf_id).terms_of_service_text
-        except AttributeError as error:
-            msg = (
-                f"Need to setup a Terms of Service Acknowledgment for {site_name}"
-            )
-            tos_html = msg
+        except (TermsOfServiceAllSites.DoesNotExist, AttributeError) as error:
             logger.info(f"{msg}\n{str(error)}")
+
+    if cur_site_curf_id is not None:
+        tos_html = TermsOfService.objects.get(curf_id=cur_site_curf_id).terms_of_service_text
 
     context = {'tos_html': tos_html}
     return render_to_string('termsofservice/tos_content.html', context)
